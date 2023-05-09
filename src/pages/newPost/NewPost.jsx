03d5b0dad549d96e52793d57342f535784c2a41d
Zhenkaaf <zhenkaaf@gmail.com>
2023-05-09 14:35:27 +0300

    validationFieldsUseRef

--- src/pages/newPost/NewPost.jsx
+++ src/pages/newPost/NewPost.jsx
@@ -2,9 +2,7 @@ import { useEffect, useState, useRef, useContext } from "react";
 import { useNavigate } from 'react-router-dom';
 import { Context } from "../../context/Context";
 import './newpost.css';
-import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import axios from "axios";
-import { storage } from "../../firebase";
 import { uploadFileToFirebase } from "../../uploadToFirebase/uploadFileToFirebase";
 
 
@@ -13,48 +11,16 @@ const NewPost = () => {
     const titleRef = useRef(null);
     const textRef = useRef(null);
     const [file, setFile] = useState(null);
+    const [isValidTitle, setValidTitle] = useState(false);
+    const [isValidText, setValidText] = useState(false);
     const { user } = useContext(Context);
     console.log('user', user);
 
-    const a = (event) => {
-        event.preventDefault();
-        fetch('http://localhost:8001/newpost', {
-            /*  fetch('https://zany-jade-chipmunk-cape.cyclic.app/newpost', { */
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'POST,'
-            },
-            /*  mode: 'cors', */
-            body: JSON.stringify({
-                title: titleRef.current.value,
-                author: user.userName,
-                text: textRef.current.value,
-                email: user.userEmail,
-            })
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw Error(response.statusText);
-                }
-                return response.text();
-            })
-            /* .then(response => {
-                console.log(typeof response);
-                return JSON.parse(response)
-            }) */
-            .then(data => {
-                console.log(data);
-                navigate('/');
-            })
-            .catch(error => console.error(error));
-    };
 
     const uploadPost = async (post) => {
         try {
-            /* const response = await axios.post('https://zany-jade-chipmunk-cape.cyclic.app/posts/newpost', post, { */
-            const response = await axios.post('http://localhost:8001/posts/newpost', post, {
+            const response = await axios.post('https://zany-jade-chipmunk-cape.cyclic.app/posts/newpost', post, {
+                /* const response = await axios.post('http://localhost:8001/posts/newpost', post, { */
                 headers: {
                     'Content-Type': 'application/json',
                     'Access-Control-Allow-Origin': '*',
@@ -68,7 +34,138 @@ const NewPost = () => {
         }
     };
 
-    /* const sendData = (event) => {
+
+
+    const checkData = () => {
+        let isValidData = true;
+
+        if (!titleRef?.current.value.trim()) {
+            setValidTitle(true);
+            isValidData = false;
+        }
+        if (!textRef?.current.value.trim()) {
+            setValidText(true);
+            isValidData = false;
+        }
+       
+        return isValidData;
+        
+    };
+
+
+    const sendData = async (event) => {
+        event.preventDefault();
+        if (!checkData()) {
+            return;
+        }
+
+        if (file) {
+            try {
+                let downloadURL = await uploadFileToFirebase(file, 'blogPhotos');
+                const post = {
+                    title: titleRef.current.value,
+                    author: user.userName,
+                    text: textRef.current.value,
+                    email: user.userEmail,
+                    picture: downloadURL,
+                };
+                await uploadPost(post);
+            } catch (err) {
+                console.error(err);
+            }
+        } else {
+            const post = {
+                title: titleRef.current.value,
+                author: user.userName,
+                text: textRef.current.value,
+                email: user.userEmail,
+            };
+            try {
+                await uploadPost(post);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    };
+
+
+
+
+    return (
+        <div className="newPost">
+            <div className="newPost__wrapper">
+                <h1 className="newPost__header">Write your own post!</h1>
+                <form id="newPost__form" method="post">
+
+                    {isValidTitle && <span className="newPost__warning">*required field</span>}
+                    <input type='text' /* defaultValue={title} */ autoFocus className="newPost__title" ref={titleRef} placeholder='Title' onChange={() => setValidTitle(false)} />
+
+                    {isValidText && <span className="newPost__warning">*required field</span>}
+                    <textarea name="text" className="newPost__text" ref={textRef} placeholder='Write your story' onChange={() => setValidText(false)}></textarea>
+
+                    <label htmlFor="newPost__fileInput">Choose photo:
+                        <i className="newPost__icon fa-regular fa-image"></i>
+                    </label>
+                    <input type="file" id='newPost__fileInput' style={{ display: 'none' }} onChange={(e) => setFile(e.target.files[0])} />
+
+                    <div className="newPost__form-block">
+                        <button className="newPost__btn" type="submit" onClick={(e) => sendData(e)} >Submit</button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default NewPost;
+
+
+
+
+/* const a = (event) => {
+    event.preventDefault();
+    fetch('http://localhost:8001/newpost', {
+         //fetch('https://zany-jade-chipmunk-cape.cyclic.app/newpost', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST,'
+        },
+         mode: 'cors',
+        body: JSON.stringify({
+            title: titleRef.current.value,
+            author: user.userName,
+            text: textRef.current.value,
+            email: user.userEmail,
+        })
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            return response.text();
+        })
+        .then(response => {
+            console.log(typeof response);
+            return JSON.parse(response)
+        })
+        .then(data => {
+            console.log(data);
+            navigate('/');
+        })
+        .catch(error => console.error(error));
+}; */
+
+
+
+
+
+
+
+
+/* const sendData = (event) => {
         event.preventDefault();
         if (file) {
             const filename = `${Date.now()}${file.name}`;
@@ -115,64 +212,3 @@ const NewPost = () => {
             uploadPost(post);
         }
     }; */
-
-    const sendData = async (event) => {
-        event.preventDefault();
-        if (file) {
-            try {
-                let downloadURL = await uploadFileToFirebase(file, 'blogPhotos');
-                const post = {
-                    title: titleRef.current.value,
-                    author: user.userName,
-                    text: textRef.current.value,
-                    email: user.userEmail,
-                    picture: downloadURL,
-                };
-                await uploadPost(post);
-            } catch (err) {
-                console.error(err);
-            }
-        } else {
-            const post = {
-                title: titleRef.current.value,
-                author: user.userName,
-                text: textRef.current.value,
-                email: user.userEmail,
-            };
-            try {
-                await uploadPost(post);
-            } catch (err) {
-                console.error(err);
-            }
-        }
-    };
-
-
-
-
-    return (
-        <div className="newPost">
-            <div className="newPost__wrapper">
-                <h1 className="newPost__header">Write your own post!</h1>
-                <form id="newPost__form" method="post">
-
-                    <input type='text' /* defaultValue={title} */ autoFocus className="newPost__title" ref={titleRef} placeholder='Title' />
-
-                    <textarea name="text" className="newPost__text" ref={textRef} placeholder='Write your story'></textarea>
-
-                    <label htmlFor="newPost__fileInput">Choose photo:
-                        <i className="newPost__icon fa-regular fa-image"></i>
-                    </label>
-                    <input type="file" id='newPost__fileInput' style={{ display: 'none' }} onChange={(e) => setFile(e.target.files[0])} />
-
-                    <div className="newPost__form-block">
-                        <button className="newPost__btn" type="submit" onClick={(e) => sendData(e)} >Submit</button>
-                    </div>
-
-
-                </form>
-            </div>
-        </div>
-    )
-}
-export default NewPost;
